2c63f9f54b363e0ec00ff1d8242d0526e25786ca
palakbansal3233 <palakbansal3233@gmail.com>
2026-03-15 18:06:20 +0530

    chore: Added About Page

--- components/About.js
+++ components/About.js
@@ -1,10 +1,13 @@
+import User from "./User";
+
 const About = () => {
-	return (
-		<>
-			<h1>About Us Page</h1>
-			<h2>More</h2>
-		</>
-	)
+  return (
+    <>
+      <h1>About Us Page</h1>
+      <h2>More</h2>
+      <User />
+    </>
+  )
 }
 
 export default About;
